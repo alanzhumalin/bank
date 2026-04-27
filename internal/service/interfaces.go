68baff5c7f975e8e7692148f243664c4784fa3e5
alan <zhumalyalan@gmail.com>
2026-04-27 18:38:41 +0500

    add interface currency repository and service

--- internal/service/interfaces.go
+++ internal/service/interfaces.go
@@ -8,8 +8,16 @@ import (
 )
 
 type UserService interface {
-	CreateUser(ctx context.Context, req dto.CreateUserRequest) error
+	Create(ctx context.Context, req dto.CreateUserRequest) error
 	Delete(ctx context.Context, id int) error
 	Update(ctx context.Context, user domain.User) error
 	GetByPhone(ctx context.Context, phone string) (dto.GetUserByPhoneResponse, error)
+}
+
+type CurrencyService interface {
+	Create(ctx context.Context, req dto.CreateNewCurrencyRequest) error
+	Delete(ctx context.Context, id int) error
+	Update(ctx context.Context, id int, req dto.UpdateCurrency) error
+	GetAll(ctx context.Context) ([]dto.GetCurrencyResponse, error)
+	GetById(ctx context.Context, id int) (dto.GetCurrencyResponse, error)
 }
