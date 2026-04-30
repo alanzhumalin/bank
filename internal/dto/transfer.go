c7ae8595fbdfd8ca3764f759d62798c564430fe9
alan <zhumalyalan@gmail.com>
2026-04-30 20:16:33 +0500

    add new function construction

--- internal/dto/transfer.go
+++ internal/dto/transfer.go
@@ -33,6 +33,45 @@ func (c *CreateTransferRequest) Validate() error {
 	return nil
 }
 
+func NewTransferFromDb(id int, senderId int, receiverId int, currency domain.Сurrency, currencyId int, amount float64, sender domain.User, receiver domain.User, status string, created_at time.Time, statusMessage string) domain.Transfer {
+	return domain.Transfer{
+		Id:                id,
+		SenderAccountId:   senderId,
+		ReceiverAccountId: receiverId,
+		CurrencyId:        currencyId,
+		Amount:            amount,
+		Status:            status,
+		CreatedAt:         created_at,
+		StatusMessage:     statusMessage,
+		Sender:            sender,
+		Receiver:          receiver,
+		Currency:          currency,
+	}
+}
+
+func NewTransfer(senderId int, receiverId int, currencyId int, amount float64) domain.Transfer {
+	return domain.Transfer{
+		SenderAccountId:   senderId,
+		ReceiverAccountId: receiverId,
+		CurrencyId:        currencyId,
+		Amount:            amount,
+	}
+}
+
+/*
+CREATE type transfer_status as enum ('pending', 'completed', 'failed');
+
+CREATE TABLE IF NOT EXISTS transfers(
+    id BIGINT generated always as identity PRIMARY key,
+    sender_account_id BIGINT not null REFERENCES accounts(id),
+    receiver_account_id BIGINT not null REFERENCES accounts(id),
+    currency_id BIGINT not null REFERENCES currencies(id),
+    amount numeric(12,2) not null check (amount > 0),
+    status transfer_status not null DEFAULT 'pending',
+    created_at TIMESTAMPtz not null default now()
+);
+*/
+
 type TransferResponse struct {
 	Id                int       `json:"id"`
 	SenderAccountId   int       `json:"sender_account_id"`
