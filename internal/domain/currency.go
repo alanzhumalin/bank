dc76c77780d2f22aa27f70bdef12f6c9c0da18e4
alan <zhumalyalan@gmail.com>
2026-04-30 15:32:20 +0500

    fix

--- internal/domain/currency.go
+++ internal/domain/currency.go
@@ -2,18 +2,6 @@ package domain
 
 import "time"
 
-/*
-CREATE TABLE IF NOT EXISTS CURRENCIES(
-
-	id bigint generated always as identity primary key,
-	name text not null,
-	code char(3) not null unique,
-	symbol VARCHAR(5) not null,
-	created_at TIMESTAMPtz not null DEFAULT now()
-
-);
-*/
-
 type Сurrency struct {
 	Id        int
 	Name      string
