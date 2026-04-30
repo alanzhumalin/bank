e2c2f18bcfa200aece471fb95789d0376bbded6f
alan <zhumalyalan@gmail.com>
2026-04-30 15:34:03 +0500

    add validation

--- internal/dto/transfer.go
+++ internal/dto/transfer.go
@@ -1,13 +1,28 @@
 package dto
 
-import (
-	"github.com/alanzhumalin/bank/internal/domain"
-)
-
 type CreateTransferRequest struct {
-	SenderAccountId   int           `json:"sender_account_id"`
-	ReceiverAccountId int           `json:"receiver_account_id"`
-	CurrencyId        int           `json:"currency_id"`
-	Amount            int           `json:"amount"`
-	Status            domain.Status `json:"status"`
+	SenderAccountId   int `json:"sender_account_id"`
+	ReceiverAccountId int `json:"receiver_account_id"`
+	CurrencyId        int `json:"currency_id"`
+	Amount            int `json:"amount"`
+}
+
+func (c *CreateTransferRequest) Validate() error {
+	if c.SenderAccountId <= 0 {
+		return ErrorSenderIdRequired
+	}
+
+	if c.ReceiverAccountId <= 0 {
+		return ErrorReceiverIdRequired
+	}
+
+	if c.CurrencyId <= 0 {
+		return ErrorCurrencyIdRequired
+	}
+
+	if c.Amount <= 0 {
+		return ErrorAmountRequired
+	}
+
+	return nil
 }
