aa0092c7f165f35197b12a71ea1c617687662923
alan <zhumalyalan@gmail.com>
2026-04-30 15:33:08 +0500

    add new errors

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -1,6 +1,8 @@
 package domain
 
-import "errors"
+import (
+	"errors"
+)
 
 var (
 	ErrorUserAlreadyExists = errors.New("User already exists")
@@ -11,4 +13,15 @@ var (
 var (
 	ErrorCurrencyAlreadyExists = errors.New("Currency already exists")
 	ErrorCurrencyNotFound      = errors.New("Currency not found")
+)
+
+var (
+	ErrorNotEnoughBalance     = errors.New("Not enough money")
+	AccountNotFound           = errors.New("Account not found")
+	AccountNotSupportCurrency = errors.New("Account does not support this currency")
+	AccountIsNotActive        = errors.New("Account is not active")
+)
+
+var (
+	ErrorTransferNotFound = errors.New("Transfer not found")
 )
