8fb8a6928cbafac844661f9845dddc111cdb5096
alan <zhumalyalan@gmail.com>
2026-04-30 20:14:37 +0500

    add new functions for account

--- internal/dto/account.go
+++ internal/dto/account.go
@@ -1,8 +1,34 @@
 package dto
 
+import (
+	"time"
+
+	"github.com/alanzhumalin/bank/internal/domain"
+)
+
 type CreateAccountRequest struct {
 	UserId     int `json:"user_id"`
 	CurrencyId int `json:"currency_id"`
+}
+
+type GetAccountResponse struct {
+	Id         int       `json:"id"`
+	UserId     int       `json:"user_id"`
+	CurrencyId int       `json:"currency_id"`
+	Balance    float64   `json:"balance"`
+	IsActive   bool      `json:"is_active"`
+	CreatedAt  time.Time `json:"created_at"`
+}
+
+func ToGetAccountResponse(a domain.Account) GetAccountResponse {
+	return GetAccountResponse{
+		Id:         a.Id,
+		UserId:     a.UserId,
+		CurrencyId: a.CurrencyId,
+		Balance:    a.Balance,
+		IsActive:   a.IsActive,
+		CreatedAt:  a.CreatedAt,
+	}
 }
 
 func (c *CreateAccountRequest) Validate() error {
@@ -15,4 +41,11 @@ func (c *CreateAccountRequest) Validate() error {
 	}
 
 	return nil
+}
+
+func (c *CreateAccountRequest) ToDomainModel() domain.Account {
+	return domain.Account{
+		UserId:     c.UserId,
+		CurrencyId: c.CurrencyId,
+	}
 }
