5a7bc0dc2071901ba2b8e5408169bf6b5f954aa6
alan <zhumalyalan@gmail.com>
2026-05-01 17:44:24 +0500

    add domain model transaction

---
+++ internal/domain/transaction.go
@@ -0,0 +1,42 @@
+package domain
+
+import (
+	"time"
+
+	"github.com/shopspring/decimal"
+)
+
+// DO $$
+// BEGIN
+//     create type transaction_type as enum ('transfer', 'deposit','withdraw');
+// EXCEPTION
+//     when duplicate_object then null;
+// END $$;
+
+// DO $$
+// BEGIN
+//     create type transaction_status as enum('pending', 'failed', 'completed');
+// EXCEPTION
+//     when duplicate_object then null;
+// END $$;
+
+// create table if not exists transactions(
+//
+//	id BIGINT generated always as identity PRIMARY KEY,
+//	type transaction_type not null,
+//	amount numeric(12,2) not null check (amount >0),
+//	account_id BIGINT not null REFERENCES accounts(id),
+//	status transaction_status not null DEFAULT 'pending',
+//	status_message text not null,
+//	created_at TIMESTAMPtz not null DEFAULT now()
+//
+// );
+type Transaction struct {
+	Id            int
+	Type          string
+	Amount        decimal.Decimal
+	AccountId     int
+	Status        string
+	StatusMessage string
+	CreatedAt     time.Time
+}
