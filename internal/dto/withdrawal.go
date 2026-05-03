944fd989fce4e0563a1598faa834ded227021c30
alan <zhumalyalan@gmail.com>
2026-05-04 00:14:49 +0500

    add dto domain model withdrawal

---
+++ internal/dto/withdrawal.go
@@ -0,0 +1,33 @@
+package dto
+
+import "github.com/shopspring/decimal"
+
+// create table if not exists withdrawals(
+//     id BIGINT generated always as identity PRIMARY KEY,
+//     transaction_id BIGINT REFERENCES transactions(id) not null,
+//     account_id BIGINT REFERENCES accounts(id) not NULL,
+//     amount numeric(12,2) not null check (amount > 0),
+//     source withdraw_source not null
+// );
+
+type CreateWindrawalRequest struct {
+	AccountId int             `json:"account_id"`
+	Amount    decimal.Decimal `json:"amount"`
+	Source    string          `json:"source"`
+}
+
+func (c *CreateWindrawalRequest) Validate() error {
+	if c.AccountId <= 0 {
+		return ErrorAccountIdRequired
+	}
+	if c.Amount.LessThanOrEqual(decimal.Zero) {
+		return ErrorAmountRequired
+	}
+
+	if c.Source == "" {
+		return ErrorSourceRequired
+	}
+
+	return nil
+
+}
