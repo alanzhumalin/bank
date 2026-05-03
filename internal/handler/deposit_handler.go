dd5cfdd42c8338eb54d9e5426d1261420e27c3a5
alan <zhumalyalan@gmail.com>
2026-05-03 22:04:28 +0500

    add create function to deposit handler

--- internal/handler/deposit_handler.go
+++ internal/handler/deposit_handler.go
@@ -1,8 +1,13 @@
 package handler
 
 import (
+	"encoding/json"
+	"errors"
 	"net/http"
+	"strconv"
 
+	"github.com/alanzhumalin/bank/internal/domain"
+	"github.com/alanzhumalin/bank/internal/dto"
 	"github.com/alanzhumalin/bank/internal/service"
 	"github.com/rs/zerolog"
 )
@@ -17,12 +22,44 @@ func NewDepositHandler(s service.DepositService, l zerolog.Logger) *depositHandl
 	return &depositHandler{service: s, logger: l.With().Str("component", "deposit_handler").Logger()}
 }
 
-func DepositRouter(d *depositHandler) http.Handler {
-	mux := http.NewServeMux()
-	mux.HandleFunc("POST /{id}/deposit", d.Create)
-	return mux
-}
-
 func (d *depositHandler) Create(w http.ResponseWriter, r *http.Request) {
+	var req dto.CreateDepositRequest
 
+	pathId := r.PathValue("account_id")
+
+	if pathId == "" {
+		WriteError(w, http.StatusBadRequest, "account id is required")
+		return
+	}
+
+	id, err := strconv.Atoi(pathId)
+
+	if err != nil {
+		WriteError(w, http.StatusBadRequest, "id must be an integer")
+		return
+	}
+
+	err = json.NewDecoder(r.Body).Decode(&req)
+
+	if err != nil {
+		WriteError(w, http.StatusBadRequest, "invalid json")
+		return
+	}
+
+	err = d.service.Create(r.Context(), req, id)
+	if err != nil {
+		switch {
+		case errors.Is(err, domain.AccountNotFound):
+			WriteError(w, http.StatusNotFound, err.Error())
+		case errors.Is(err, domain.AccountIsNotActive):
+			WriteError(w, http.StatusNotFound, err.Error())
+		default:
+			d.logger.Error().Err(err).Str("account_id", pathId).Msg("Error occured")
+			WriteError(w, http.StatusInternalServerError, "internal server error")
+			return
+		}
+	}
+
+	d.logger.Info().Str("account_id", pathId).Str("amount", req.Amount.String()).Msg("Created deposit")
+	WriteJson(w, http.StatusCreated, "created")
 }
