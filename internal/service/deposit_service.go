b815dd9c71a1dfa24def87d2e4b07780fff30122
alan <zhumalyalan@gmail.com>
2026-05-03 22:05:21 +0500

    add create function deposit service

--- internal/service/deposit_service.go
+++ internal/service/deposit_service.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 
+	"github.com/alanzhumalin/bank/internal/domain"
 	"github.com/alanzhumalin/bank/internal/dto"
 	"github.com/alanzhumalin/bank/internal/repository"
 )
@@ -23,8 +24,53 @@ func NewDepositService(depoRepo repository.DepositRepository, accountRepo reposi
 	}
 }
 
-func (ds *depositService) Create(ctx context.Context, req dto.CreateDepositRequest) error {
+func (ds *depositService) Create(ctx context.Context, req dto.CreateDepositRequest, id int) error {
 	return ds.txManager.WithTx(ctx, func(ctx context.Context) error {
+
+		acc, err := ds.accountRepo.GetByIdForUpdate(ctx, id)
+
+		if err != nil {
+			return err
+		}
+
+		if !acc.IsActive {
+			return domain.AccountIsNotActive
+		}
+
+		transactionId, err := ds.transactionRepo.Create(ctx, domain.Transaction{
+			Type:          "deposit",
+			Amount:        req.Amount,
+			AccountId:     id,
+			Status:        "pending",
+			StatusMessage: "transaction created",
+		})
+
+		if err != nil {
+			return err
+		}
+
+		err = ds.accountRepo.IncreaseBalance(ctx, req.Amount, id)
+
+		if err != nil {
+			return err
+		}
+
+		err = ds.depositRepo.Create(ctx, domain.Deposit{
+			TransactionId: transactionId,
+			AccountId:     id,
+			Amount:        req.Amount,
+			Source:        req.Source,
+		})
+
+		if err != nil {
+			return err
+		}
+
+		err = ds.transactionRepo.MarkTransaction(ctx, "completed", "transaction completed", transactionId)
+
+		if err != nil {
+			return err
+		}
 
 		return nil
 	})
