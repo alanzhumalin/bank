575fc4109eb9152b7fe960114204652304f78115
alan <zhumalyalan@gmail.com>
2026-05-05 17:15:29 +0500

    change the transaction structure, add new fields that best describe every transactions

--- internal/domain/transaction.go
+++ internal/domain/transaction.go
@@ -6,37 +6,39 @@ import (
 	"github.com/shopspring/decimal"
 )
 
-// DO $$
-// BEGIN
-//     create type transaction_type as enum ('transfer', 'deposit','withdraw');
-// EXCEPTION
-//     when duplicate_object then null;
-// END $$;
-
-// DO $$
-// BEGIN
-//     create type transaction_status as enum('pending', 'failed', 'completed');
-// EXCEPTION
-//     when duplicate_object then null;
-// END $$;
-
-// create table if not exists transactions(
-//
-//	id BIGINT generated always as identity PRIMARY KEY,
-//	type transaction_type not null,
-//	amount numeric(12,2) not null check (amount >0),
-//	account_id BIGINT not null REFERENCES accounts(id),
-//	status transaction_status not null DEFAULT 'pending',
-//	status_message text not null,
-//	created_at TIMESTAMPtz not null DEFAULT now()
-//
-// );
 type Transaction struct {
-	Id            int
-	Type          string
-	Amount        decimal.Decimal
-	AccountId     int
-	Status        string
-	StatusMessage string
-	CreatedAt     time.Time
+	Id             int
+	Type           string
+	Amount         decimal.Decimal
+	AccountId      int
+	Status         string
+	StatusMessage  string
+	CurrencyId     int
+	CurrencyCode   string
+	CurrencySymbol string
+
+	WithDrawalDetail *WithdrawalDetail
+	TransferDetail   *TransferDetail
+	DepositDetail    *DepositDetail
+
+	CreatedAt time.Time
+}
+
+type DepositDetail struct {
+	Source string
+}
+
+type TransferDetail struct {
+	Sender   UserDetail
+	Receiver UserDetail
+}
+
+type UserDetail struct {
+	FirstName   string
+	LastName    string
+	PhoneNumber string
+}
+
+type WithdrawalDetail struct {
+	Source string
 }
