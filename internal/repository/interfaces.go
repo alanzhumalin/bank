5ec9360a6084d8ae25a8930367c3cd4c71d0430f
alan <zhumalyalan@gmail.com>
2026-05-07 16:47:37 +0500

    add role returning

--- internal/repository/interfaces.go
+++ internal/repository/interfaces.go
@@ -11,7 +11,7 @@ import (
 )
 
 type UserRepository interface {
-	Create(ctx context.Context, u user.User) (int, error)
+	Create(ctx context.Context, u user.User) (int, string, error)
 	UserExists(ctx context.Context, phoneNumber string) error
 	Delete(ctx context.Context, id int) error
 	Update(ctx context.Context, user domain.User) error
