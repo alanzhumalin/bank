9d05c833e6da6b3303c47f622b3c80e8daec1fa2
alan <zhumalyalan@gmail.com>
2026-05-10 22:44:03 +0500

    add rbac middleware

---
+++ internal/middleware/rbac.go
@@ -0,0 +1,34 @@
+package middleware
+
+import (
+	"net/http"
+	"slices"
+
+	"github.com/alanzhumalin/bank/internal/dto"
+	"github.com/alanzhumalin/bank/pkg/response"
+)
+
+type RbacMiddleware struct{}
+
+func NewRbacMiddleware() *RbacMiddleware {
+	return &RbacMiddleware{}
+}
+
+func (m *RbacMiddleware) RBAC(roles ...string) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			role, ok := r.Context().Value(dto.RoleKey{}).(string)
+			if !ok {
+				response.WriteError(w, http.StatusForbidden, "forbidden")
+				return
+			}
+
+			if ok := slices.Contains(roles, role); ok {
+				next.ServeHTTP(w, r)
+				return
+			}
+
+			response.WriteError(w, http.StatusForbidden, "forbidden")
+		})
+	}
+}
