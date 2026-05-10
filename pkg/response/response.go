830ec0aeb4a6af4fa9c6b1ccf4b03ea1f20cc150
alan <zhumalyalan@gmail.com>
2026-05-10 22:43:49 +0500

    replaced the http response

---
+++ pkg/response/response.go
@@ -0,0 +1,22 @@
+package response
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func WriteJson(w http.ResponseWriter, status int, message any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(map[string]any{
+		"data": message,
+	})
+}
+
+func WriteError(w http.ResponseWriter, status int, message any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(map[string]any{
+		"error": message,
+	})
+}
