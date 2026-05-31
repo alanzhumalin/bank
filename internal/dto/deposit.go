8efa0faeb2143791fa129a405762006f1c94ebff
alan <zhumalyalan@gmail.com>
2026-05-31 12:32:26 +0500

    add validation

--- internal/dto/deposit.go
+++ internal/dto/deposit.go
@@ -6,3 +6,15 @@ type CreateDepositRequest struct {
 	Amount decimal.Decimal `json:"amount"`
 	Source string          `json:"source"`
 }
+
+func (c *CreateDepositRequest) Validate() error {
+	if c.Amount.IsZero() {
+		return ErrorAmountIsRequired
+	}
+
+	if c.Source == "" {
+		return ErrorDepositSourceIsRequired
+	}
+
+	return nil
+}
