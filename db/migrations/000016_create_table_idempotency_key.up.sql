e4f9dcdb88095dea04ded858c774c27213304e60
alan <zhumalyalan@gmail.com>
2026-06-02 15:01:59 +0500

    add idempotency migration

---
+++ db/migrations/000016_create_table_idempotency_key.up.sql
@@ -0,0 +1,19 @@
+DO $$
+BEGIN
+    create type operation_type as enum ('withdraw', 'deposit', 'transfer');
+EXCEPTION
+    when duplicate_object then NULL;
+END $$;
+
+create table if not exists idempotency_keys(
+    id BIGINT generated always as identity PRIMARY KEY,
+    transaction_id BIGINT REFERENCES transactions(id),
+    user_id BIGINT REFERENCES users(id) not null,
+    idempotency_key text not null,
+    operation operation_type not null,
+    status text not null DEFAULT 'pending',
+    response JSONB,
+    created_at TIMESTAMPTZ not null DEFAULT now(),
+    updated_at TIMESTAMPTZ not null,
+    UNIQUE(user_id, idempotency_key)
+);
